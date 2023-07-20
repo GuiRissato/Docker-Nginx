70d85e74dab7dfa2dee374a0849fcad7ed0c5f7b
Guilherme Rissato <guilherme_andrade_rissato@hotmail.com>
2023-07-20 18:55:43 -0300

    database update

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE people (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO people (name) VALUES ('Guilherme');
+INSERT INTO people (name) VALUES ('Wesley');
